bbc46beaf1df813d94fdc6911bde105afb08c518
c1311152 Kilmer Luiz <c1311152@bb.com.br>
2020-06-06 12:43:06 -0300

    Create models (investment, portfolio).

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_06_153649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "investments", force: :cascade do |t|
+    t.string "name"
+    t.float "asset_amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "portfolio_id", null: false
+    t.index ["portfolio_id"], name: "index_investments_on_portfolio_id"
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "investments", "portfolios"
 end
